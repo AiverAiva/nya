709f14883ce6f1c0c2f1e68c26257e55d62dcb8a
AiverAiva <yaminocreeper@gmail.com>
2024-01-04 05:28:23 +0800

    perf: make the rank tag using emoji instead of plain text

--- cogs/wynncraft/player.py
+++ cogs/wynncraft/player.py
@@ -9,7 +9,8 @@ import datetime
 import sys
 sys.path.append("..")
 import utilties.embeds as embeds
-import ui.wynncraft as wynncraft
+from utilties.wynncraft import ui, colors
+
 from utilties.multicog import add_to_group
 from utilties.database import getUserData
 
@@ -24,7 +25,6 @@ class player(commands.Cog):
     async def player(self, ctx, name :Option(str, "The name of the player you want to search for.", required=False)):
         if name is None:
             userdata = await getUserData(ctx.author.id)
-            print(userdata)
             dbname = userdata['wynncraft']
             if dbname is None:
                 return await ctx.respond(embed=embeds.Error(f"You don't have an account connected to the bot, use </wynncraft link:1160063178907062342> to link"))
@@ -38,17 +38,29 @@ class player(commands.Cog):
     
         button_overall = Button(label="Overall", style=discord.ButtonStyle.blurple, emoji="👥")
         async def overall_callback(interaction):
-            await interaction.response.edit_message(embed=wynncraft.overall(data))
+            await interaction.response.edit_message(embed=ui.overall(data), view=view)
         button_overall.callback = overall_callback
     
         button_dungeonandraid = Button(label="Dungeon&Raid", style=discord.ButtonStyle.blurple, emoji="🗝️")
         async def dungeonandraid_callback(interaction):
-            await interaction.response.edit_message(embed=wynncraft.dungeonandraid(data))
+            await interaction.response.edit_message(embed=ui.dungeonandraid(data), view=view)
         button_dungeonandraid.callback = dungeonandraid_callback
-    
-        view = View(button_overall, button_dungeonandraid)
-    
-        await ctx.respond(embed=wynncraft.overall(data), view=view)
+
+        button_classes = Button(label="Classes", style=discord.ButtonStyle.blurple, emoji="🗝️")
+        async def classes_callback(interaction):
+            chooseClass = ui.defaultPlayerEmbed(data)
+            chooseClass.description = "Choose a class to conitnue:",
+            await interaction.response.edit_message(embed=chooseClass, view=selectMenuview)
+        button_classes.callback = classes_callback
+        
+        async def select_callback(interaction):
+            await interaction.response.edit_message(embed=ui.classOverlay(data, selectMenu.values[0]))
+
+        selectMenu = ui.classSelectMenu(data)
+        selectMenu.callback = select_callback
+        view = View(button_overall, button_dungeonandraid, button_classes)
+        selectMenuview = View(button_overall, button_dungeonandraid, button_classes, selectMenu)
+        await ctx.respond(embed=ui.overall(data), view=view)
 
 def setup(bot):
     bot.add_cog(player(bot))
